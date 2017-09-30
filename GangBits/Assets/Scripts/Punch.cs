299424391197bf3f6a0f30a208fe0a163a389011
Dominic Hawkins <dominic2.hawkins@live.uwe.ac.uk>
2017-09-30 02:19:02 +0100

    Added basic punch movement, doesn't reset position properly

--- GangBits/Assets/Scripts/Punch.cs
+++ GangBits/Assets/Scripts/Punch.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Punch : MonoBehaviour {
+    public GameObject rightHand;
+    public GameObject leftHand;
     // Use this for initialization
-	void Start () {
+    void Start () {
     }
     
     // Update is called once per frame
     void Update () {
-
-
+        if (Input.GetMouseButton(0))
+        {
+                rightHand.transform.localPosition = new Vector3(Mathf.PingPong(Time.time * 3.0f, 0.8f), rightHand.transform.localPosition.y, rightHand.transform.localPosition.z);
+                leftHand.transform.localPosition = new Vector3(Mathf.PingPong(Time.time * 2.5f, 0.8f), leftHand.transform.localPosition.y, leftHand.transform.localPosition.z);
+        }
+        else
+        {
+            //Have Position Reset that considers position
         }
     }
+}
