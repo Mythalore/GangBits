0d36bd0492d4b19c96f201630c245571ead8487e
Mythalore <samfrichards@gmail.com>
2017-10-02 22:27:02 +0100

    Truck Level Complete!

---
+++ GangBits/Assets/Levels/Truck/TruckLevel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckLevel : MonoBehaviour {
+
+	public Animator sign, trucks;
+	public float signTime, truckChangeTime;
+	public float signCount, truckChangeCount;
+	public bool isFrontTruck;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		signCount += Time.deltaTime;
+		if(signCount >= signTime){
+			sign.SetTrigger ("SIGN!");
+			signCount = 0;
+		}
+		truckChangeCount += Time.deltaTime;
+		if(truckChangeCount >= truckChangeTime){
+			if (isFrontTruck) {
+				trucks.SetTrigger ("FrontSwitch");
+				isFrontTruck = false;
+			} else {
+				trucks.SetTrigger ("BackSwitch");
+				isFrontTruck = true;
+			}
+			truckChangeCount = 0;
+		}
+	}
+}
