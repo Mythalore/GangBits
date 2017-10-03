93f70cf611dd4db2f95fa890af1d92aa695e9fc2
Ellie Richards <eleanorichards@hotmail.co.uk>
2017-10-03 12:59:49 +0100

    Code Polish
    Punching independently
    Side arm movement still affecting every player in scene

--- GangBits/Assets/Scripts/Punch.cs
+++ GangBits/Assets/Scripts/Punch.cs
@@ -6,6 +6,12 @@ public class Punch : MonoBehaviour {
     //Right Facing
     public GameObject rightHand;
     public GameObject leftHand;
+
+	public GameObject Player1;
+	public GameObject Player2;
+	public GameObject Player3;
+	public GameObject Player4;
+
     private Transform RightrightHandStore;
     private Transform RightleftHandStore;
     private Vector3 rightLeftHandvec;
@@ -17,8 +23,11 @@ public class Punch : MonoBehaviour {
     //-     -       -       -       -
     //Outside Function
     private Direction directionFunc;
+	private string player_name = "";
+	private string punch_button = "";
     // Use this for initialization
     void Start () {
+
         directionFunc = GetComponent<Direction>(); // Gets the direction the player is facing
 
         RightrightHandStore = rightHand.transform; //Stores the inital values of the hands to move them to resetted positions
@@ -28,30 +37,50 @@ public class Punch : MonoBehaviour {
 
         LeftLeftHandvec = new Vector3(-RightleftHandStore.localPosition.x, RightleftHandStore.localPosition.y, RightleftHandStore.localPosition.z); //Location of left hand when facing left
         LeftRightHandvec = new Vector3(-RightrightHandStore.localPosition.x, RightrightHandStore.localPosition.y, RightrightHandStore.localPosition.z); //Location of right hand when facing left
+
+		if (this.gameObject.tag == "Player1") {
+			player_name = "Player1";
+			punch_button = "joystick 1 button 0";
+		}
+		if (this.gameObject.tag == "Player2") {
+			player_name = "Player2";
+			punch_button = "joystick 2 button 0";
+		}
+		if (this.gameObject.tag == "Player3") {
+			player_name = "Player3";
+			punch_button = "joystick 3 button 0";
+		}
+		if (this.gameObject.tag == "Player4") {
+			player_name = "Player4";
+			punch_button = "joystick 4 button 0";
+		}
+
     }
 
     // Update is called once per frame
     void Update () { //Joystick button 0 is the A button on the controller
-        if (Input.GetKey("joystick button 0") && directionFunc.FacingLeft == false) //Right Facing Punch Animation
-        {
-            rightHand.transform.localPosition = new Vector3(Mathf.PingPong(Time.time * 3.0f, 0.8f), rightHand.transform.localPosition.y, rightHand.transform.localPosition.z);
-            leftHand.transform.localPosition = new Vector3(Mathf.PingPong(Time.time * 2.5f, 0.8f), leftHand.transform.localPosition.y, leftHand.transform.localPosition.z);
-        }
-        else if (Input.GetKey("joystick button 0") && directionFunc.FacingLeft == true) //Left Facing Punch Animation
-        {
-            rightHand.transform.localPosition = new Vector3(Mathf.PingPong(Time.time * 3.0f, -0.5f), rightHand.transform.localPosition.y, rightHand.transform.localPosition.z);
-            leftHand.transform.localPosition = new Vector3(Mathf.PingPong(Time.time * 2.5f, -0.5f), leftHand.transform.localPosition.y, leftHand.transform.localPosition.z);
-        }
-        else if (Input.GetKeyUp("joystick button 0") && directionFunc.FacingLeft == false) //Right Facing Position reset
-        {
-            rightHand.transform.localPosition = rightRightHandvec;
-            leftHand.transform.localPosition = rightLeftHandvec;
-        }
-        else if (Input.GetKeyUp("joystick button 0") && directionFunc.FacingLeft == true) //Left Facing Position reset
-        {
-            rightHand.transform.localPosition = LeftRightHandvec;
-            leftHand.transform.localPosition = LeftLeftHandvec;
+		if (this.gameObject.tag == player_name) {
+			if (Input.GetKey(punch_button) && directionFunc.FacingLeft == false) //Right Facing Punch Animation
+			{
+				this.rightHand.transform.localPosition = new Vector3(Mathf.PingPong(Time.time * 3.0f, 0.8f), rightHand.transform.localPosition.y, rightHand.transform.localPosition.z);
+				this.leftHand.transform.localPosition = new Vector3(Mathf.PingPong(Time.time * 2.5f, 0.8f), leftHand.transform.localPosition.y, leftHand.transform.localPosition.z);
+			}
+			else if (Input.GetKey(punch_button) && directionFunc.FacingLeft == true) //Left Facing Punch Animation
+			{
+				this.rightHand.transform.localPosition = new Vector3(Mathf.PingPong(Time.time * 3.0f, -0.5f), rightHand.transform.localPosition.y, rightHand.transform.localPosition.z);
+				this.leftHand.transform.localPosition = new Vector3(Mathf.PingPong(Time.time * 2.5f, -0.5f), leftHand.transform.localPosition.y, leftHand.transform.localPosition.z);
+			}
+			else if (Input.GetKeyUp(punch_button) && directionFunc.FacingLeft == false) //Right Facing Position reset
+			{
+				this.rightHand.transform.localPosition = rightRightHandvec;
+				this.leftHand.transform.localPosition = rightLeftHandvec;
+			}
+				else if (Input.GetKeyUp(punch_button) && directionFunc.FacingLeft == true) //Left Facing Position reset
+			{
+				this.rightHand.transform.localPosition = LeftRightHandvec;
+				this.leftHand.transform.localPosition = LeftLeftHandvec;
+			}
+		}
 
-        }
     }
 }
